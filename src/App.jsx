4a2083b17c9dbc700d63d0c62d662bc783d67bc2
charan-innosify <charan@innosify.com>
2026-01-09 18:29:31 +0530

    chore: Setup automatic deployment to Hostinger via FTP
    - Updated deploy.yml to use FTP-Deploy-Action
    - Removed unused files (RadarSystem.jsx, email.service.js, helper.js)
    - Package and App updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,69 @@
-/**
- * KolmoTek Instruments Website
- * @authors R.CHARAN & S ANANDH
- * @company Innosify Pvt Ltd
- */
-
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
-import ProductGroupPage from "./pages/ProductGroupPage";
-import CodeOfConduct from "./pages/CodeOfConduct";
-import TermsAndConditions from "./pages/TermsAndCondition";
+
+// Fallback component for Suspense
+const Loading = () => (
+  <div className="flex items-center justify-center min-h-[60vh]">
+    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-sky-500"></div>
+  </div>
+);
+
+// Lazy loaded components
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Contact = lazy(() => import("./pages/Contact"));
+const ProductGroupPage = lazy(() => import("./pages/ProductGroupPage"));
+const CodeOfConduct = lazy(() => import("./pages/CodeOfConduct"));
+const TermsAndConditions = lazy(() => import("./pages/TermsAndCondition"));
+const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
 
 // Laser Based System Products:
-import LaserInterferometer from "./pages/ProductGroup/Product/laserInterferometer";
-import PIVSystem from "./pages/ProductGroup/Product/PIVSystem";
-import LIDARSystem from "./pages/ProductGroup/Product/LightDetectionAndRanging";
-import LIBSSystem from "./pages/ProductGroup/Product/LaserInducedBreakdownSpectroscopy";
-import DICSystem from "./pages/ProductGroup/Product/dicSystem";
-import TLSSystem from "./pages/ProductGroup/Product/TerrestrialLaserScanningSystem";
+const LaserInterferometer = lazy(() => import("./pages/ProductGroup/Product/laserInterferometer"));
+const PIVSystem = lazy(() => import("./pages/ProductGroup/Product/PIVSystem"));
+const LIDARSystem = lazy(() => import("./pages/ProductGroup/Product/LightDetectionAndRanging"));
+const LIBSSystem = lazy(() => import("./pages/ProductGroup/Product/LaserInducedBreakdownSpectroscopy"));
+const DICSystem = lazy(() => import("./pages/ProductGroup/Product/dicSystem"));
+const TLSSystem = lazy(() => import("./pages/ProductGroup/Product/TerrestrialLaserScanningSystem"));
 
 // Acoustic Instruments Products:
-import EchoSounder from "./pages/ProductGroup/Product/EchoSounder";
-import AcousticDopplerCurrentProfiler from "./pages/ProductGroup/Product/AcousticDopplerCurrentProfiler";
-import AcousticDopplerVelocimeter from "./pages/ProductGroup/Product/AcousticDopplerVelocimeter";
+const EchoSounder = lazy(() => import("./pages/ProductGroup/Product/EchoSounder"));
+const AcousticDopplerCurrentProfiler = lazy(() => import("./pages/ProductGroup/Product/AcousticDopplerCurrentProfiler"));
+const AcousticDopplerVelocimeter = lazy(() => import("./pages/ProductGroup/Product/AcousticDopplerVelocimeter"));
 
-// Acoustic Instruments Products:
-import UnmannedAerialVehicle from "./pages/ProductGroup/Product/UnmannedAerialVehicle";
-import AutonomousAerialVehicles from "./pages/ProductGroup/Product/AutonomousAerialVehicles";
+// Aerial Survey Products:
+const UnmannedAerialVehicle = lazy(() => import("./pages/ProductGroup/Product/UnmannedAerialVehicle"));
+const AutonomousAerialVehicles = lazy(() => import("./pages/ProductGroup/Product/AutonomousAerialVehicles"));
 
 // UnderWater Survey Systems Products:
-import AutonomousUnderwaterVehicles from "./pages/ProductGroup/Product/AutonomousUnderwaterVehicles";
-import RemotelyOperatedVehicles from "./pages/ProductGroup/Product/RemotelyOperatedVehicles";
+const AutonomousUnderwaterVehicles = lazy(() => import("./pages/ProductGroup/Product/AutonomousUnderwaterVehicles"));
+const RemotelyOperatedVehicles = lazy(() => import("./pages/ProductGroup/Product/RemotelyOperatedVehicles"));
 
 // RF Microwave Systems Products:
-import RadioDetectionRangingSystem from "./pages/ProductGroup/Product/RadioDetectionRangingSystem";
-import SeaSurfaceRadar from "./pages/ProductGroup/Product/SeaSurfaceRadar";
+const RadioDetectionRangingSystem = lazy(() => import("./pages/ProductGroup/Product/RadioDetectionRangingSystem"));
+const SeaSurfaceRadar = lazy(() => import("./pages/ProductGroup/Product/SeaSurfaceRadar"));
 
 // Strain Gauge System Products:
-import LoadCell from "./pages/ProductGroup/Product/LoadCell";
-import PressureSensor from "./pages/ProductGroup/Product/PressureSensors";
+const LoadCell = lazy(() => import("./pages/ProductGroup/Product/LoadCell"));
+const PressureSensor = lazy(() => import("./pages/ProductGroup/Product/PressureSensors"));
 
-// import PressureSensors from "./pages/Product/PressureSensors";
-import Application from "./pages/Application";
-import ApplicationPage from "./pages/Application";
-import FlowVisualizationPage from "./pages/Application/FlowVisualization";
-import HydrographicSurveyPage from "./pages/Application/HydrographicSurvey";
-import AerialSurveyPage from "./pages/Application/AerialSurveyPage";
-import StructuralHealthMonitoringPage from "./pages/Application/StructuralHealthMonitoring";
-import DeepSeaExplorationPage from "./pages/Application/DeepSeaExploration";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import LaserBasedProductsPage from "./pages/ProductGroup/LaserBasedSystem";
-import AcousticInstrumentsPage from "./pages/ProductGroup/AcousticInstruments";
-import AerialSurveySystemsPage from "./pages/ProductGroup/AerialSurveySystems";
-import UnderWaterSurveySystemsPage from "./pages/ProductGroup/UnderWaterSurveySystem";
-import RFMicrowaveSystemsPage from "./pages/ProductGroup/RF&MicrowaveSystem";
-import StrainGaugeSystemPage from "./pages/ProductGroup/StrainGaugeSystems";
+// Application Pages:
+const Application = lazy(() => import("./pages/Application"));
+const ApplicationPage = lazy(() => import("./pages/Application"));
+const FlowVisualizationPage = lazy(() => import("./pages/Application/FlowVisualization"));
+const HydrographicSurveyPage = lazy(() => import("./pages/Application/HydrographicSurvey"));
+const AerialSurveyPage = lazy(() => import("./pages/Application/AerialSurveyPage"));
+const StructuralHealthMonitoringPage = lazy(() => import("./pages/Application/StructuralHealthMonitoring"));
+const DeepSeaExplorationPage = lazy(() => import("./pages/Application/DeepSeaExploration"));
+
+const LaserBasedProductsPage = lazy(() => import("./pages/ProductGroup/LaserBasedSystem"));
+const AcousticInstrumentsPage = lazy(() => import("./pages/ProductGroup/AcousticInstruments"));
+const AerialSurveySystemsPage = lazy(() => import("./pages/ProductGroup/AerialSurveySystems"));
+const UnderWaterSurveySystemsPage = lazy(() => import("./pages/ProductGroup/UnderWaterSurveySystem"));
+const RFMicrowaveSystemsPage = lazy(() => import("./pages/ProductGroup/RF&MicrowaveSystem"));
+const StrainGaugeSystemPage = lazy(() => import("./pages/ProductGroup/StrainGaugeSystems"));
 
 const App = () => {
   return (
@@ -67,151 +71,153 @@ const App = () => {
       <ScrollToTop />
       <Navbar />
       <main className="flex-1 bg-white">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-          <Route path="/CodeOfConduct" element={<CodeOfConduct />} />
-          <Route path="/TermsAndConditions" element={<TermsAndConditions />} />
-          <Route path="/ProductGroup" element={<ProductGroupPage />} />
-          <Route
-            path="/ProductGroup/LaserInstruments"
-            element={<LaserBasedProductsPage />}
-          />
-          <Route
-            path="/ProductGroup/AcousticInstruments"
-            element={<AcousticInstrumentsPage />}
-          />
-          <Route
-            path="/ProductGroup/AerialSurveySystems"
-            element={<AerialSurveySystemsPage />}
-          />
-          <Route
-            path="/ProductGroup/UnderWaterSurveySystems"
-            element={<UnderWaterSurveySystemsPage />}
-          />
-          <Route
-            path="/ProductGroup/RFMicrowaveInstruments"
-            element={<RFMicrowaveSystemsPage />}
-          />
-          <Route
-            path="/ProductGroup/StrainGaugeInstruments"
-            element={<StrainGaugeSystemPage />}
-          />
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+            <Route path="/CodeOfConduct" element={<CodeOfConduct />} />
+            <Route path="/TermsAndConditions" element={<TermsAndConditions />} />
+            <Route path="/ProductGroup" element={<ProductGroupPage />} />
+            <Route
+              path="/ProductGroup/LaserInstruments"
+              element={<LaserBasedProductsPage />}
+            />
+            <Route
+              path="/ProductGroup/AcousticInstruments"
+              element={<AcousticInstrumentsPage />}
+            />
+            <Route
+              path="/ProductGroup/AerialSurveySystems"
+              element={<AerialSurveySystemsPage />}
+            />
+            <Route
+              path="/ProductGroup/UnderWaterSurveySystems"
+              element={<UnderWaterSurveySystemsPage />}
+            />
+            <Route
+              path="/ProductGroup/RFMicrowaveInstruments"
+              element={<RFMicrowaveSystemsPage />}
+            />
+            <Route
+              path="/ProductGroup/StrainGaugeInstruments"
+              element={<StrainGaugeSystemPage />}
+            />
 
-          {/* Laser Based System Products: */}
-          <Route
-            path="/ProductGroup/LaserInstruments/LaserInterferometer"
-            element={<LaserInterferometer />}
-          />
-          <Route
-            path="/ProductGroup/LaserInstruments/LIDARSystem"
-            element={<LIDARSystem />}
-          />
-          <Route
-            path="/ProductGroup/LaserInstruments/PIVSystem"
-            element={<PIVSystem />}
-          />
-          <Route
-            path="/ProductGroup/LaserInstruments/LIBSSystem"
-            element={<LIBSSystem />}
-          />
-          <Route
-            path="/ProductGroup/LaserInstruments/DICSystem"
-            element={<DICSystem />}
-          />
-          <Route
-            path="/ProductGroup/LaserInstruments/TLSSystem"
-            element={<TLSSystem />}
-          />
+            {/* Laser Based System Products: */}
+            <Route
+              path="/ProductGroup/LaserInstruments/LaserInterferometer"
+              element={<LaserInterferometer />}
+            />
+            <Route
+              path="/ProductGroup/LaserInstruments/LIDARSystem"
+              element={<LIDARSystem />}
+            />
+            <Route
+              path="/ProductGroup/LaserInstruments/PIVSystem"
+              element={<PIVSystem />}
+            />
+            <Route
+              path="/ProductGroup/LaserInstruments/LIBSSystem"
+              element={<LIBSSystem />}
+            />
+            <Route
+              path="/ProductGroup/LaserInstruments/DICSystem"
+              element={<DICSystem />}
+            />
+            <Route
+              path="/ProductGroup/LaserInstruments/TLSSystem"
+              element={<TLSSystem />}
+            />
 
-          {/* Acoustic Instruments Products: */}
+            {/* Acoustic Instruments Products: */}
 
-          <Route
-            path="/AcousticInstruments/EchoSounder"
-            element={<EchoSounder />}
-          />
-          <Route
-            path="/AcousticInstruments/AcousticDopplerCurrentProfiler"
-            element={<AcousticDopplerCurrentProfiler />}
-          />
-          <Route
-            path="/AcousticInstruments/AcousticDopplerVelocimeter"
-            element={<AcousticDopplerVelocimeter />}
-          />
+            <Route
+              path="/AcousticInstruments/EchoSounder"
+              element={<EchoSounder />}
+            />
+            <Route
+              path="/AcousticInstruments/AcousticDopplerCurrentProfiler"
+              element={<AcousticDopplerCurrentProfiler />}
+            />
+            <Route
+              path="/AcousticInstruments/AcousticDopplerVelocimeter"
+              element={<AcousticDopplerVelocimeter />}
+            />
 
-          {/* Aerial Survey Systems Page: */}
+            {/* Aerial Survey Systems Page: */}
 
-          <Route
-            path="/AerialSurveySystems/UnmannedAerialVehicle"
-            element={<UnmannedAerialVehicle />}
-          />
+            <Route
+              path="/AerialSurveySystems/UnmannedAerialVehicle"
+              element={<UnmannedAerialVehicle />}
+            />
 
-          <Route
-            path="/AerialSurveySystems/AutonomousAerialVehicles"
-            element={<AutonomousAerialVehicles />}
-          />
+            <Route
+              path="/AerialSurveySystems/AutonomousAerialVehicles"
+              element={<AutonomousAerialVehicles />}
+            />
 
-          {/* Under Water Survey System: */}
+            {/* Under Water Survey System: */}
 
-          <Route
-            path="/UnderWaterSurveySystems/AutonomousUnderwaterVehicles"
-            element={<AutonomousUnderwaterVehicles />}
-          />
+            <Route
+              path="/UnderWaterSurveySystems/AutonomousUnderwaterVehicles"
+              element={<AutonomousUnderwaterVehicles />}
+            />
 
-          <Route
-            path="/UnderWaterSurveySystems/RemotelyOperatedVehicles"
-            element={<RemotelyOperatedVehicles />}
-          />
+            <Route
+              path="/UnderWaterSurveySystems/RemotelyOperatedVehicles"
+              element={<RemotelyOperatedVehicles />}
+            />
 
-          {/* RF & Microwave System: */}
-          <Route
-            path="/RFMicrowaveInstruments/RadioDetectionRangingInstruments"
-            element={<RadioDetectionRangingSystem />}
-          />
-          <Route
-            path="/RFMicrowaveInstruments/SeaSurfaceRadar"
-            element={<SeaSurfaceRadar />}
-          />
+            {/* RF & Microwave System: */}
+            <Route
+              path="/RFMicrowaveInstruments/RadioDetectionRangingInstruments"
+              element={<RadioDetectionRangingSystem />}
+            />
+            <Route
+              path="/RFMicrowaveInstruments/SeaSurfaceRadar"
+              element={<SeaSurfaceRadar />}
+            />
 
-          {/* Strain Gauge System: */}
-          <Route
-            path="/StrainGaugeInstruments/LoadCell"
-            element={<LoadCell />}
-          />
-          <Route
-            path="/StrainGaugeInstruments/PressureSensors"
-            element={<PressureSensor />}
-          />
+            {/* Strain Gauge System: */}
+            <Route
+              path="/StrainGaugeInstruments/LoadCell"
+              element={<LoadCell />}
+            />
+            <Route
+              path="/StrainGaugeInstruments/PressureSensors"
+              element={<PressureSensor />}
+            />
 
-          <Route path="/application" element={<Application />} />
+            <Route path="/application" element={<Application />} />
 
-          <Route path="/Application" element={<ApplicationPage />} />
-          <Route
-            path="/Application/FlowVisualization"
-            element={<FlowVisualizationPage />}
-          />
-          <Route
-            path="/Application/HydrographicSurvey"
-            element={<HydrographicSurveyPage />}
-          />
-          <Route
-            path="/Application/AerialSurveyPage"
-            element={<AerialSurveyPage />}
-          />
-          <Route
-            path="/Application/StructuralHealthMonitoring"
-            element={<StructuralHealthMonitoringPage />}
-          />
-          <Route
-            path="/Application/DeepSeaExploration"
-            element={<DeepSeaExplorationPage />}
-          />
-        </Routes>
+            <Route path="/Application" element={<ApplicationPage />} />
+            <Route
+              path="/Application/FlowVisualization"
+              element={<FlowVisualizationPage />}
+            />
+            <Route
+              path="/Application/HydrographicSurvey"
+              element={<HydrographicSurveyPage />}
+            />
+            <Route
+              path="/Application/AerialSurveyPage"
+              element={<AerialSurveyPage />}
+            />
+            <Route
+              path="/Application/StructuralHealthMonitoring"
+              element={<StructuralHealthMonitoringPage />}
+            />
+            <Route
+              path="/Application/DeepSeaExploration"
+              element={<DeepSeaExplorationPage />}
+            />
+          </Routes>
+        </Suspense>
       </main>
       <Footer />
-    </div >
+    </div>
   );
 };
 
--- src/services/email.service.js
+++
@@ -1,6 +0,0 @@
-import { customAxios } from "./helper";
-
-export async function sendEmail(emailData){
-    const result = (await customAxios.post(`/email/send`, emailData)).data;
-    return result;
-}
